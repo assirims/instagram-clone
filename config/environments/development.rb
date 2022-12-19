4287f4411b15ebdfa07e5ebfe4b4b6ddf5974194
Mohammed Assiri <q2me@live.com>
2022-12-19 19:47:12 +0000

    set up sign up and in

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,16 +33,14 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # 
-  
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  
+
   config.action_mailer.perform_caching = false
-  
+
   # add for devise gem
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
