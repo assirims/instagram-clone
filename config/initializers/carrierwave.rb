7a0bfc260b6ac2177dd412e060d4c173366b56e8
Mohammed Assiri <q2me@live.com>
2022-12-23 18:21:31 +0000

    hide some info

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     'xxx',                        # required unless using use_iam_profile
+      aws_access_key_id:     '111',                        # required unless using use_iam_profile
       aws_secret_access_key: 'yyy',                        # required unless using use_iam_profile
       use_iam_profile:       true,                         # optional, defaults to false
       region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
